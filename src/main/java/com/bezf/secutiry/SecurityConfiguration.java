675bb20ac75682967343ffe4f6f45b5602144006
VeraLizunova <vera.lizunova.98@mail.ru>
2022-12-16 18:15:16 +0100

    secure methods

--- src/main/java/com/bezf/secutiry/SecurityConfiguration.java
+++ src/main/java/com/bezf/secutiry/SecurityConfiguration.java
@@ -1,30 +1,32 @@
 package com.bezf.secutiry;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Ivanov").password("{noop}IvanovPassword").authorities("read")
-                .and()
-                .withUser("Petrov").password("{noop}PetrovPassword").authorities("read", "write");
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Ivanov").password(encoder().encode("IvanovPassword"))
+                .roles("READ")
                 .and()
-                .authorizeRequests().antMatchers("/hello").permitAll()
+                .withUser("Petrov").password(encoder().encode("PetrovPassword"))
+                .roles("WRITE")
                 .and()
-                .authorizeRequests().antMatchers("/write").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers("/read").hasAuthority("read")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .withUser("Sidorov").password(encoder().encode("SidorovPassword"))
+                .roles("READ", "DELETE");
     }
 }
 
